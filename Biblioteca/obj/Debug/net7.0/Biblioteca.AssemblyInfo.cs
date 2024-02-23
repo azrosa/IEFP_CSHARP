c7e21f8d4083e92cb3856ee01d7e697d9cfa76c6
Alan Rosa <azevedo.rosa@hotmail.com>
2024-02-23 20:02:52 -0300

    versao

--- Biblioteca/obj/Debug/net7.0/Biblioteca.AssemblyInfo.cs
+++ Biblioteca/obj/Debug/net7.0/Biblioteca.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e08003a7713455b7180ff46e377b904330844753")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+405731e64645620beb76e5a096812d06ac0c0b26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Biblioteca")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
